967f61766a276a81b80de9bbb2b87a6dff3297cb
Camille Anelli <camilleanelli32@gmail.com>
2016-04-27 17:19:37 +0200

    refactoring & extract method

--- societecom.rb
+++ societecom.rb
@@ -1,9 +1,23 @@
 require 'open-uri'
 require 'nokogiri'
 
+
+def open_url(url)
+  Nokogiri::HTML(open(url))
+end
+
+def fetch_juridic_table_row(number, url)
+  document = open_url(url)
+  document.css("table#rensjur tr")[number].css('td')[1].text
+end
+
 def fetch_siren(url)
-  doc = Nokogiri::HTML(open(url))
-  doc.css("table#rensjur tr")[3].css('td')[1].text
+  fetch_juridic_table_row( 3, url)
+end
+
+def fetch_siret(url)
+  fetch_juridic_table_row(4, url)
 end
 
 puts fetch_siren("http://www.societe.com/societe/fullsix-france-438769127.html")
+puts fetch_siret("http://www.societe.com/societe/fullsix-france-438769127.html")
